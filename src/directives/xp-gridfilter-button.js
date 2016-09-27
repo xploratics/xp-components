8ee00745c9f2a5c805a9fe343984a3b0a90a3928
Dany Laporte <dany_laporte@hotmail.com>
2016-09-26 21:00:36 -0400

    add highlight when grid is filtered

--- src/directives/xp-gridfilter-button.js
+++ src/directives/xp-gridfilter-button.js
@@ -1,12 +1,24 @@
-module.exports = ['xpGridfilterDialog', function (xpGridfilterDialog) {
+module.exports = ['xpGridfilterDialog', '$parse', function (xpGridfilterDialog, parse) {
     return {
         scope: true,
-        template: `<md-button aria-label="Filter" ng-click="showDialog()"><ng-md-icon icon="filter_list"></ng-md-icon></md-button>`,
+        template: `<md-button aria-label="Filter" ng-click="showDialog()" ng-class="{'md-primary': filtered()}">
+            <ng-md-icon icon="filter_list"></ng-md-icon>
+        </md-button>`,
         link: function (scope, element, attrs) {
+            var options = parse(attrs.xpGridOptions);
+
+            scope.filtered = function () {
+                var { columnDefs } = options(scope) || 0;
+                if (columnDefs)
+                    for (var i = 0; i < columnDefs.length; i++)
+                        if (columnDefs[i].filters && columnDefs[i].filters.length)
+                            return true;
+            };
+
             scope.showDialog = function () {
-                var gridOptions = scope.$eval(attrs.xpGridOptions || '{}');
-                xpGridfilterDialog({ gridOptions })
-            }
+                var gridOptions = options(scope) || {};
+                xpGridfilterDialog({ gridOptions });
+            };
         }
     };
 }];
