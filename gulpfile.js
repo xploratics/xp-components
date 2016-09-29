80bb6365d9431a3eefbf974414d6882fb61336de
Dany Laporte <dany_laporte@hotmail.com>
2016-09-28 21:19:55 -0400

    Add gulp in package.json

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,6 @@ var gulp = require('gulp');
 gulp.task('build', ['build:css', 'build:js']);
 gulp.task('watch', ['watch:css', 'watch:js']);
 
-gulp.task('build:publish', ['build', 'bump']);
-gulp.task('build:publish:minor', ['build', 'bump:minor']);
-gulp.task('build:publish:major', ['build', 'bump:major']);
+gulp.task('publish', ['build', 'bump']);
+gulp.task('publish:minor', ['build', 'bump:minor']);
+gulp.task('publish:major', ['build', 'bump:major']);
