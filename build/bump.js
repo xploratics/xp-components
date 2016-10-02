2a168b54ae2be5476d50079321949c43cd158a0b
Dany Laporte <dany_laporte@hotmail.com>
2016-10-02 16:38:55 -0400

    add support for commiting output

--- build/bump.js
+++ build/bump.js
@@ -2,12 +2,17 @@ var bump = require('gulp-bump');
 var filter = require('gulp-filter');
 var git = require('gulp-git');
 var gulp = require('gulp');
+var merge_stream = require('merge-stream');
 var tag_version = require('gulp-tag-version');
 
 function inc(importance) {
-    return gulp.src(['./package.json', './bower.json'])
+    var packages = gulp.src(['./package.json', './bower.json'])
         .pipe(bump({type: importance}))
-        .pipe(gulp.dest('./'))
+        .pipe(gulp.dest('./'));
+
+    var outputs = gulp.src(['./dist/**/*.*']);
+
+    return merge_stream(packages, outputs)
         .pipe(git.commit('bumps package version'))
         .pipe(filter('package.json'))
         .pipe(tag_version());
